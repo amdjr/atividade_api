fde4b836683283b89c3175f02ed9fdf5580c729f
Adilson Monteiro <adilson_monteiro@outlook.com>
2021-10-09 22:48:42 -0300

    comit ok

--- src/main/java/br/adilson/api/com/atividade_api/controller/HomeController.java
+++ src/main/java/br/adilson/api/com/atividade_api/controller/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String index() {
-        return "# One Punch Man # by @AdilsonMonteiron";
+        return "# One Punch Man # by @Adilson_Monteiro";
     }
 }
